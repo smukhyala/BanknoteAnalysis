b35bcf3b7a09378fa60a0e6899c3614de602c50d
Sanjay Mukhyala <45247680+smukhyala@users.noreply.github.com>
2024-01-25 21:49:21 -0800

    Update main.py (cleanup)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 #gathering file
 data = pd.read_csv('em-9EhjTEemU7w7-EFnPcg_7aa34fc018d311e980c2cb6467517117_happyscore_income.csv')
@@ -8,30 +10,24 @@ happy = data['happyScore']
 income = data['avg_income']
 inequality = data['income_inequality']
 
-import matplotlib.pyplot as plt
-
+#set up pyplot structure
 plt.xlabel('income')
 plt.ylabel('happy score')
 plt.scatter(income, happy)
 
-import numpy as np
-
+#basic data sorting
 data.sort_values('avg_income', inplace=True)
-
 richest = data[ data['avg_income'] > 15000 ]
-
 richest.iloc[-1]
-
 rich_mean = np.mean(richest['avg_income'])
-
 all_mean = np.mean(data['avg_income'])
 
+#creating a second scatter for the happiness of the highest income countries
 plt.scatter(richest['avg_income'], richest['happyScore'])
 
 plt.text(richest.iloc[0]['avg_income'], 
          richest.iloc[0]['happyScore'], 
          richest.iloc[0]['country'])
-
 plt.text(richest.iloc[-1]['avg_income'], 
          richest.iloc[-1]['happyScore'], 
          richest.iloc[-1]['country'])
@@ -39,24 +35,10 @@ plt.text(richest.iloc[-1]['avg_income'],
 for k,row in richest.iterrows():
     print(row['country'])
 
-data.sort_values('avg_income', inplace=True)
-
-richest = data[ data['avg_income'] > 15000 ]
-
-richest.iloc[-1]
-
-rich_mean = np.mean(richest['avg_income'])
-
-all_mean = np.mean(data['avg_income'])
-
 plt.scatter(richest['avg_income'], richest['happyScore'])
 
 for k,row in richest.iterrows():
     plt.text(row['avg_income'], row['happyScore'], row['country'])
-    
-    # print(row['country'])
-
-    
 
 #another method of plotting, using the built-in Pandas plot function:
 scatter2 = richest.plot(x='avg_income', y='happyScore', kind='scatter')
@@ -70,8 +52,8 @@ plt.xlabel('income')
 plt.ylabel('happy score')
 plt.scatter(income, happy, s = inequality*10, alpha=0.25)
 
+#using scikitplot
 from sklearn.cluster import KMeans
-
 income_happy = np.column_stack((income, happy))
 
 km_res = KMeans(n_clusters = 3).fit(income_happy)
